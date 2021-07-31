5b7d511d56e6aacd30ef620a79f86815277a7c73
MilaVit <tanimani@ukr.net>
2021-07-31 22:27:21 +0300

    method endWith

---
+++ homeWork4/endWith/script.js
@@ -0,0 +1,22 @@
+const STR = 'Hello world';
+
+function endWith (str,search) {
+    let leng = -search.length
+    let  = str.slice(leng)
+
+    if (
+        searchStr.length === "" ||
+        typeof mainStr !== "string" ||
+        typeof searchStr !== "string"
+    ) {
+        throw new Error("Data is not correct!");
+    }
+
+    if(q !== search) {
+        return false
+    } else {
+        return true
+    }
+}
+
+console.log(endWith(STR, 'world'))
